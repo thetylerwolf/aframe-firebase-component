5ff19ca5c47220eafb4fbd31e98006fe1177fd57
Kevin Ngo <me@ngokevin.com>
2016-07-26 16:44:08 -0700

    fix recursion error, wait for parent

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var FirebaseWrapper = require('./firebaseWrapper');
 
+var getComponentProperty = AFRAME.utils.entity.getComponentProperty;
+var setComponentProperty = AFRAME.utils.entity.setComponentProperty;
+
 if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available.');
 }
@@ -21,7 +24,8 @@ AFRAME.registerSystem('firebase', {
     // Get config.
     var config = this.data;
 
-    if (!config.apiKey) { return; }
+    // TODO: https://github.com/aframevr/aframe/pull/1670
+    if (!config.apiKey && !window.debug) { return; }
 
     this.broadcastingEntities = {};
     this.entities = {};
@@ -56,17 +60,28 @@ AFRAME.registerSystem('firebase', {
     // Already added.
     if (this.entities[id] || this.broadcastingEntities[id]) { return; }
 
+    // Handle parent-child relationships.
+    var parentId = data.parentId;
+    var parentEl = this.sceneEl;
+    if (parentId) {
+      parentEl = this.entities[parentId] || this.sceneEl.querySelector('#' + parentId);
+      if (!parentEl) {
+        // Wait for parent to attach. (TODO: use Promises).
+        var self = this;
+        return setTimeout(function () {
+          self.handleEntityAdded(id, data);
+        });
+      }
+    }
+    delete data.parentId;
+
+    // Create and reference entity.
     var entity = document.createElement('a-entity');
     this.entities[id] = entity;
 
-    // Parent node.
-    var parentId = data.parentId;
-    var parentEl = this.entities[parentId] || this.sceneEl;
-    delete data.parentId;
-
     // Components.
     Object.keys(data).forEach(function setComponent (componentName) {
-      AFRAME.utils.entity.setComponentProperty(entity, componentName, data[componentName]);
+      setComponentProperty(entity, componentName, data[componentName]);
     });
 
     parentEl.appendChild(entity);
@@ -82,7 +97,7 @@ AFRAME.registerSystem('firebase', {
     var entity = this.entities[id];
     Object.keys(components).forEach(function setComponent (componentName) {
       if (componentName === 'parentId') { return; }
-      setAttribute(entity, componentName, components[componentName]);
+      setComponentProperty(entity, componentName, components[componentName]);
     });
   },
 
@@ -104,8 +119,11 @@ AFRAME.registerSystem('firebase', {
 
     // Initialize entry, get assigned a Firebase ID.
     var id = this.firebaseWrapper.createEntity();
-    el.setAttribute('firebase-broadcast', 'id', id);
-    broadcastingEntities[id] = el;
+
+    setTimeout(function () {
+      broadcastingEntities[id] = el;
+      el.setAttribute('firebase-broadcast', 'id', id);
+    });
 
     // Remove entry when client disconnects.
     this.firebaseWrapper.removeEntityOnDisconnect(id);
@@ -126,7 +144,7 @@ AFRAME.registerSystem('firebase', {
 
     Object.keys(broadcastingEntities).forEach(function broadcast (id) {
       var el = broadcastingEntities[id];
-      var components = el.getAttribute('firebase-broadcast').components;
+      var components = el.getComputedAttribute('firebase-broadcast').components;
       var data = {};
 
       // Add components to broadcast once.
@@ -144,7 +162,7 @@ AFRAME.registerSystem('firebase', {
 
       // Build data.
       components.forEach(function getData (componentName) {
-        data[componentName] = AFRAME.utils.entity.getComponentProperty(el, componentName, '|');
+        data[componentName] = getComponentProperty(el, componentName, '|');
       });
 
       // Update entry.
@@ -163,10 +181,11 @@ AFRAME.registerComponent('firebase-broadcast', {
     componentsOnce: {default: [], type: 'array'}
   },
 
-  init: function () {
+  init: function (oldData) {
     var data = this.data;
     var el = this.el;
     var system = el.sceneEl.systems.firebase;
+
     if (data.components.length) {
       system.registerBroadcast(el);
     }
